c94031fe137ed45e0f3c63edc4966edc5244b8bc
cankatmis <ackatmis@gmail.com>
2022-12-24 23:43:07 +0100

    Update ArrayADTCPP.cpp

--- ArrayADTCPP/ArrayADTCPP.cpp
+++ ArrayADTCPP/ArrayADTCPP.cpp
@@ -75,11 +75,10 @@ void Array<T>::Insert(int index, T x) {
 template<class T>
 T Array<T>::Delete(int index) {
 	T x = 0;
-	int i;
 
 	if (index >= 0 && index < length) {
 		x = A[index];
-		for (i = index;i < length - 1;i++)
+		for (int i = index;i < length - 1;i++)
 			A[i] = A[i + 1];
 		length--;
 		return x;
@@ -157,8 +156,8 @@ void Array<T>::Set(int index, T x) {
 template<class T>
 T Array<T>::Max() {
 	T max = A[0];
-	int i;
-	for (i = 1;i < length;i++) {
+	
+	for (int i = 1;i < length;i++) {
 		if (A[i] > max)
 			max = A[i];
 	}
@@ -168,8 +167,8 @@ T Array<T>::Max() {
 template<class T>
 T Array<T>::Min() {
 	T min = A[0];
-	int i;
-	for (i = 1;i < length;i++) {
+	
+	for (int i = 1;i < length;i++) {
 		if (A[i] < min)
 			min = A[i];
 	}
@@ -179,8 +178,8 @@ T Array<T>::Min() {
 template<class T>
 T Array<T>::Sum() {
 	T s = 0;
-	int i;
-	for (i = 0;i < length;i++)
+	
+	for (int i = 0;i < length;i++)
 		s += A[i];
 	return s;
 }
